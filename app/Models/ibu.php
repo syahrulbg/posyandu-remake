3a5337e694202ba8b994532e328c6672ab26cffc
syahrulbg <134019061+syahrulbg@users.noreply.github.com>
2023-05-22 14:29:46 +0700

    Update ibu.php

--- app/Models/ibu.php
+++ app/Models/ibu.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ibu extends Model
 {
+    
     use HasFactory;
     protected $fillable = ['id', 'name', 'alamat', 'nomorHP', 'ttl', 'jenis_kelamin'];
     public function user()
